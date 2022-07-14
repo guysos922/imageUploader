5df657af41ed5df8f9803fc2ab21c71a0055973c
seanMilver <seanmilver3@gmail.com>
2022-07-14 19:11:06 +0300

    the html server with css files

--- backEnd.py
+++ backEnd.py
@@ -1,7 +1,16 @@
-
-def main():
-    x = 0
-    print(x + 1)
+from flask import Flask , render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///images.db'
 
+
+@app.route("/")
+def servP():
+    return render_template("index.html")
+    
+
+
+if __name__ == "__main__":
+    app.run()
