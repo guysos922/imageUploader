87f2f29f06e9faf8b7d8fdd1ba7516afc35d3b01
guysos922 <guy4uk@gmail.com>
2022-07-14 20:26:34 +0300

    added browse button and drop image zone

---
+++ main.js
@@ -0,0 +1,30 @@
+function dropHandler(ev) {
+    console.log('File(s) dropped');
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      for (let i = 0; i < ev.dataTransfer.items.length; i++) {
+        // If dropped items aren't files, reject them
+        if (ev.dataTransfer.items[i].kind === 'file') {
+          const file = ev.dataTransfer.items[i].getAsFile();
+          console.log('... file[' + i + '].name = ' + file.name);
+        }
+      }
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      for (let i = 0; i < ev.dataTransfer.files.length; i++) {
+        console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+      }
+    }
+  }
+
+  function dragOverHandler(ev) {
+    document.getElementsByClassName('drop-image-rect')[0].style.border = '1px solid rgba(0, 0, 0, 0.35)';
+  }
+
+  function dragLeaveHandler(ev) {
+    document.getElementsByClassName('drop-image-rect')[0].style.border = '1px dashed rgba(0, 0, 0, 0.35)';
+  }
