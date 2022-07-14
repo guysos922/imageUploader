fc772ec06adc1e6e0059f36d4c5791f0f0df4122
seanMilver <seanmilver3@gmail.com>
2022-07-14 20:00:58 +0300

    the first time the project united

--- backEnd.py
+++ backEnd.py
@@ -2,13 +2,25 @@ from flask import Flask , render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///images.db'
 
+'''
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+'''
+def static_dir(path):
+    return send_from_directory("static", path)
 
-@app.route("/")
+
+@app.route('/')
 def servP():
     return render_template("index.html")
+
+
     
 
 
